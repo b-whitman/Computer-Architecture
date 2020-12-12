33a4d52b83df0206782f8e877354ec84273a19e9
Benjamin Whitman <benjamin.whitman90@gmail.com>
2020-12-11 22:16:41 -0500

    CMP and CALL

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,6 +10,11 @@ PUSH = 0b01000101
 POP  = 0b01000110
 CALL = 0b01010000
 RET  = 0b00010001
+ADD  = 0b10100000
+CMP  = 0b10100111
+JEQ  = 0b01010101
+JMP  = 0b01010100
+JNE  = 0b01010110
 
 class CPU:
     """Main CPU class."""
@@ -26,11 +31,14 @@ class CPU:
         self.branchtable[HLT] = self.handle_HLT
         self.branchtable[LDI] = self.handle_LDI
         self.branchtable[PRN] = self.handle_PRN
+        self.branchtable[ADD] = self.handle_ADD
         self.branchtable[MUL] = self.handle_MUL
         self.branchtable[PUSH] = self.handle_PUSH
         self.branchtable[POP] = self.handle_POP
         self.branchtable[CALL] = self.handle_CALL
         self.branchtable[RET] = self.handle_RET
+        self.branchtable[CMP] = self.handle_CMP
+        self.branchtable[JMP] = self.handle_JMP
 
     def load(self):
         """Load a program into memory."""
@@ -78,6 +86,15 @@ class CPU:
         #elif op == "SUB": etc
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "CMP":
+            value_a = self.reg[reg_a]
+            value_b = self.reg[reg_b]
+            if value_a == value_b:
+                self.fl = 0b00000001
+            elif value_a > value_b:
+                self.fl = 0b00000010
+            elif value_a < value_b:
+                self.fl = 0b00000100
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -111,6 +128,9 @@ class CPU:
         number_to_print = self.reg[register_address]
         print(number_to_print)
     
+    def handle_ADD(self, operand_a, operand_b):
+        self.alu('ADD', operand_a, operand_b)
+
     def handle_MUL(self, operand_a, operand_b):
         self.alu('MUL', operand_a, operand_b)
 
@@ -137,7 +157,15 @@ class CPU:
 
     def handle_CALL(self, operand_a, operand_b):
         # Push address of next instruction onto the stack
+        self.reg[7] -= 1
+        value = self.pc + 2
+        sp = self.reg[7]
+        self.ram[sp] = value
         # Set PC to address stored in given register
+        register_address = operand_a
+        ram_address = self.reg[register_address]
+        self.pc = ram_address
+
 
     def handle_RET(self, operand_a, operand_b):
         # Pop value from top of stack
@@ -146,6 +174,16 @@ class CPU:
         self.reg[7] += 1
         # Store value in PC
         self.pc = value
+
+    def handle_JMP(self, operand_a, operand_b):
+        register_address = operand_a
+        ram_address = self.reg[register_address]
+        self.pc = ram_address
+
+    def handle_CMP(self, operand_a, operand_b):
+        self.alu("CMP", operand_a, operand_b)
+
+
 
     def run(self):
         """Run the CPU."""
@@ -160,8 +198,9 @@ class CPU:
 
             operand_a = self.ram_read(ir+1)
             operand_b = self.ram_read(ir+2)
-
             self.branchtable[command](operand_a, operand_b)
 
             number_of_operands = command >> 6
-            self.pc += (1 + number_of_operands)
+            sets_pc = ((command >> 4) & 0b001)
+            if not sets_pc:
+                self.pc += (1 + number_of_operands)
